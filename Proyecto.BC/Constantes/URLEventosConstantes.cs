108cdf2f9f04a0f1d57750a1f2a1b8f270e088cb
Leonarotti <159678359+Leonarotti@users.noreply.github.com>
2024-07-05 08:57:52 -0600

    Hosting

--- Proyecto.BC/Constantes/URLEventosConstantes.cs
+++ Proyecto.BC/Constantes/URLEventosConstantes.cs
@@ -8,9 +8,6 @@ namespace Proyecto.BC.Constantes
 {
     public class URLEventosConstantes
     {
-        public const string URLServicioEventos = "https://localhost:7251/api/EventoProducto/";
-
-    //    public const string URLSagaUserAbdRolles = "https://localhost:7253/api/SagasEventos/procesar";
-    //    public const string URLSagaUserIsValide = "https://localhost:7253/api/SagasEventos/isActive/";
+        public const string URLServicioEventos = "https://servicioevento.azurewebsites.net/api/EventoProducto/";
     }
 }
